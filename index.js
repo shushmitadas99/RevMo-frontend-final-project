ac3b146130e05f1bdbafb03f4f50984dbd9d7420
Aidan Carter <aidan.kyle.carter@gmail.com>
2022-08-16 08:59:18 -0700

    update template page

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const url = "127.0.0.1";
 
 let logoutBtn = document.getElementById('logout-btn');
 let loginBtn = document.getElementById('login-btn');
+let createAccount = document.getElementById('create-account');
+let myAccount = document.getElementById('my-account');
+let transferMoney = document.getElementById('transfer-money');
 
 const burgerIcon=document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
@@ -15,13 +18,17 @@ window.addEventListener('load', (e) => {
     console.log('in window load block');
     console.log(sessionStorage.getItem('username'))
     if (sessionStorage.getItem('username') != null){
-
+        createAccount.classList.add('is-hidden');
         logoutBtn.classList.remove('is-hidden');
         loginBtn.classList.add('is-hidden');
+        myAccount.classList.remove('is-hidden');
+        transferMoney.classList.remove('is-hidden');
     } else {
-
+        createAccount.classList.remove('is-hidden');
         logoutBtn.classList.add('is-hidden');
         loginBtn.classList.remove('is-hidden');
+        myAccount.classList.add('is-hidden');
+        transferMoney.classList.add('is-hidden');
     }
 })
 logoutBtn.addEventListener('click', async (e) => 
