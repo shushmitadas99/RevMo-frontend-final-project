dc49f975bcf45897f8d02aaf593e7ea9b3e35dc9
STM2050 <shaquera.moore@gmail.com>
2022-08-25 12:49:26 -0400

    modify index.html, resetpassword.html, resetpassword.js, and style.css

--- resetpassword.js
+++ resetpassword.js
@@ -1,12 +1,39 @@
-let password = document.querySelector("#new_password").value;
-let cnfrmPassword = document.querySelector("#confirm_password").value;
-console.log(" Password:", password,'\n',"Confirm Password:",cnfrmPassword);
+let passwordInput = document.querySelector("#new_password").value;
+let cnfrmPasswordInput = document.querySelector("#confirm_password").value;
 let message = document.querySelector("#password_message");
-resetPasswordButton.addEventListner("click", resetPassword);
+document.getElementById("reset_password").addEventListener("click", resetPassword);
 
-async function resetPassword
-    try{
-        let res =await fetch(``)
+async function resetPassword() { 
+    try {
+        let res = await fetch(`http://localhost:8080/resetpassword`,{
+            method:"POST", 
+            method: "PUT",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({
+                passwordInput: passwordInput.value,
+                cnfrmPasswordInput: cnfrmPasswordInput.value
+            }),
+        });
+        if (res.status == 202){
+            let data = await res.json;
+            let password = data.password;
+            let cnfrmPassword = data.cnfrmPassword
+            
+        }
+        if (res.status == 401){
+            data = await res.json();
+            console.log("401 status gopu");
+            emailInput.value = "";
+            console.log("401 status gopu2");
+            let para = document.querySelector("#password_message");
+            console.log(`para = ${para}`);
+            para.style.color = "black";
+            para.innerHTML = data.message;
+
+        }
+    } catch (error){
+        console.log(error);
     }
-
-    
+}
