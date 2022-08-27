425f6a19a500c23415584baf202a79b557dac43e
Aidan Carter <aidan.kyle.carter@gmail.com>
2022-08-26 18:14:53 -0700

    track income page working

--- income.js
+++ income.js
@@ -3,6 +3,9 @@ let incomeTable = document.getElementById('income-table');
 let userId = sessionStorage.getItem('userId')
 console.log("user Id: ", userId)
 
+let refreshBtn = document.getElementById('refresh');
+let monthsToDisplay = document.getElementById('months-to-display');
+
 let column = document.getElementById('tables-column');
 let numMonths = 3;
 let today = new Date();
@@ -19,71 +22,101 @@ window.addEventListener('load', async (e) => {
             'Content-Type': 'application/json'
         }
     });
+    if(sessionStorage.getItem("months to display")){
+        monthsToDisplay.setAttribute("value", sessionStorage.getItem("months to display"))
+    }
     let data = await res.json();
     console.log("data");
     console.log(data);
-    console.log(data.accounts)
+    console.log(data.accounts);
     addTableForAccount(data.accounts);
     
 })
 
-
+refreshBtn.addEventListener('click', () => {
+    sessionStorage.setItem("months to display", monthsToDisplay.value);
+    location.reload();
+})
 
 console.log(today, today.getMonth()+1, today.getFullYear())
+
 
 
 async function addTableForAccount(accounts) {
     console.log(isIterable(accounts));
     if (isIterable(accounts)) {
         for (account of accounts){
+            let monthsToDo = monthsToDisplay.value;
+            // console.log("months to display", monthsToDo);
             
             let aId = account.accountId;
-            console.log("aId", aId);
+            // console.log("aId", aId);
 
-            
-            let income = await fetch (`http://${url}:8080/trx/income-by-account/${aId}/${month}/${year}`, {
-                'credentials': 'include',
-                'method': 'GET',
-                'headers': {
-                    'Access-Control-Allow-Origin':'*',
-                    'Content-Type': 'application/json'
+            let header = document.createElement('h2');
+            header.setAttribute("id", `header-acct-${aId}`);
+            header.classList.add('is-size-4');
+            header.innerHTML = `Account ${account.accountId} <span class="is-size-5">(${account.typeName})</span>`;
+            column.appendChild(header);
+            let table = document.createElement('table');
+            table.classList.add('table', 'has-text-centered', 'is-fullwidth')
+            table.setAttribute("id", `table-acct-${aId}`)
+            table.innerHTML = "<thead><tr class=\"has-text-centered\"><th>Year</th> <th>Month</th><th>Total Income</th></tr></thead>";
+            let body = document.createElement('tbody');
+            body.innerHTML = "";
+
+            for (let i = 0; i < monthsToDo; i++){
+                let thisMonth = month-i-1;
+                let thisYear = year;
+                if (thisMonth <= 0 ){
+                    thisMonth = 12+thisMonth;
+                    thisYear--;
                 }
-            });
-            let incomeData = await income.json();
-            console.log("incomeData", incomeData);
-            console.log("status", income.status)
             
-            
-            if(incomeData > -1) {
+                let income = await fetch (`http://${url}:8080/trx/income-by-account/${aId}/${thisMonth}/${year}`, {
+                    'credentials': 'include',
+                    'method': 'GET',
+                    'headers': {
+                        'Access-Control-Allow-Origin':'*',
+                        'Content-Type': 'application/json'
+                    }
+                });
+                let incomeData = await income.json();
+                // console.log("incomeData", incomeData);
+                // console.log("status", income.status)
                 
-                let header = document.createElement('h2');
-                header.classList.add('is-size-4');
-                header.innerHTML = `Account ${account.accountId} <span class="is-size-5">(${account.typeName})</span>`;
-                column.appendChild(header);
-                let table = document.createElement('table');
-                table.classList.add('table', 'has-text-centered', 'is-fullwidth')
-                table.innerHTML = "<thead><tr class=\"has-text-centered\"><th>Year</th> <th>Month</th><th>Total Income</th></tr></thead>";
-                let body = document.createElement('tbody');
-                body.innerHTML = "";
                 
+                if(incomeData > -1) {
+                    
+                   
+                    
 
-                let row = document.createElement('tr');
-                let yearCell = document.createElement('td');
-                yearCell.innerHTML = `<p>${year}</p>`;
-                let monthCell = document.createElement('td');
-                monthCell.innerHTML = `${months[month-1]}`;
-                let income = document.createElement('td');
-                income.innerHTML = `${numWCommas((incomeData/100).toFixed(2))}`;
+                    let row = document.createElement('tr');
+                    let yearCell = document.createElement('td');
+                    yearCell.innerHTML = `<p>${year}</p>`;
+                    let monthCell = document.createElement('td');
+                    monthCell.innerHTML = `${months[thisMonth]}`;
+                    let income = document.createElement('td');
+                    income.innerHTML = `${numWCommas((incomeData/100).toFixed(2))}`;
+                    
+                    row.appendChild(yearCell);
+                    row.appendChild(monthCell);
+                    row.appendChild(income);                
+                    body.appendChild(row);
                 
-                row.appendChild(yearCell);
-                row.appendChild(monthCell);
-                row.appendChild(income);                
-                body.appendChild(row);
-            
-               
                 
-                table.appendChild(body);
-                column.appendChild(table);
+                    
+                    table.appendChild(body);
+                    column.appendChild(table);
+                }
+                // else {
+                //     let header = document.getElementById(`header-acct-${aId}`);
+                //     header.classList.add("is-hidden");
+                // }
+            }
+            console.log("can find header?", document.getElementById(`table-acct-${aId}`))
+            if(!document.getElementById(`table-acct-${aId}`)){
+                let header = document.getElementById(`header-acct-${aId}`);
+                header.classList.add("is-hidden");
             }
             
             
